0e66a9a8d2b2600d2027131594be5fdfd24e0315
Timyr4ikon <54175850+Timyr4ikon@users.noreply.github.com>
2020-03-19 00:42:28 +0200

    dsf

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import './styles.css';
+import fetchCountries from './fetchCountries'
+const debounce = require('lodash.debounce');
+import '../node_modules/pnotify/dist/PNotifyBrightTheme.css'
+import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
+PNotify.defaults.width = '100%';
+
+
+
+const currentCountry = document.querySelector('input[data-action="search"]');
+const root = document.querySelector('.root');
+currentCountry.addEventListener('input',debounce(sendResult,500));
+
+function sendResult(){
+    fetchCountries(currentCountry.value)
+    .then(data =>{
+        if(data.length > 10){
+            alertError();
+        }
+        if(data.length >1 && data.length < 11){
+          root.innerHTML =  createGalleryList(data);
+        }
+        if(data.length ===1){
+            const country = data[0];
+            console.log(country.languages)
+            root.innerHTML = `     <h2>${country.name}</h2>
+            <div class="flex">
+                <div class="content">
+                  <p>Capital: <span>${country.capital}</span></p>
+                  <p>Population: <span>${country.population}</span></p>
+                  <div>
+                    <p>Languages:</p>
+                    <ul>
+                        ${lol(country.languages)}
+                    </ul>
+                  </div>
+               </div>
+      
+               <img src=${country.flag} alt="">
+          </div>`;
+                          
+        }
+        
+    })
+
+}
+function alertError(){
+    PNotify.error({
+        text: `Too many matches found.
+         Please enter a more specific query!`,
+      });
+}
+function createGalleryItem(el){
+    return  `
+    <li >
+        ${el.name}
+    </li>
+    `
+}
+function createGalleryList(data){
+    return `<ul>${data.reduce((markup, el) => markup += createGalleryItem(el), "")}</ul>`;
+  }
+function lol(langs){
+    return langs.reduce((acc,el) => acc+=`<li>${el.nativeName}</li>`,'');
+}
+
