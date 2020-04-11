fda196560c90c4a1dc21795b6d61691960a7b63d
Timyr4ikon <54175850+Timyr4ikon@users.noreply.github.com>
2020-04-11 20:29:33 +0300

    fsfs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './styles.css';
+import './gg.js'
 import fetchCountries from './fetchCountries'
 const debounce = require('lodash.debounce');
 import '../node_modules/pnotify/dist/PNotifyBrightTheme.css'
 import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
+import PNotifyButtons from '../node_modules/pnotify/dist/es/PNotifyButtons.js';
 PNotify.defaults.width = '100%';
+PNotify.defaults.delay = 1500;
 
 
 
@@ -12,36 +15,48 @@ const root = document.querySelector('.root');
 currentCountry.addEventListener('input',debounce(sendResult,500));
 
 function sendResult(){
-    fetchCountries(currentCountry.value)
-    .then(data =>{
-        if(data.length > 10){
-            alertError();
-        }
-        if(data.length >1 && data.length < 11){
-          root.innerHTML =  createGalleryList(data);
-        }
-        if(data.length ===1){
-            const country = data[0];
-            console.log(country.languages)
-            root.innerHTML = `     <h2>${country.name}</h2>
-            <div class="flex">
-                <div class="content">
-                  <p>Capital: <span>${country.capital}</span></p>
-                  <p>Population: <span>${country.population}</span></p>
-                  <div>
-                    <p>Languages:</p>
-                    <ul>
-                        ${lol(country.languages)}
-                    </ul>
-                  </div>
-               </div>
-      
-               <img src=${country.flag} alt="">
-          </div>`;
-                          
-        }
-        
-    })
+    if(currentCountry.value.length !== 0){
+        fetchCountries(currentCountry.value)
+        .then(data =>{
+            console.log(data);
+            if(data.status === 404){
+                PNotify.error({
+                    text: `Make sure to write the name of the country properly`,
+                  });
+                  return;
+            }
+            if(data.length > 10){
+                alertError();
+                return
+            }
+            if(data.length >1 && data.length < 11){
+              root.innerHTML =  createGalleryList(data);
+              return;
+            }
+            if(data.length ===1){
+                const country = data[0];
+                console.log(country.languages)
+                root.innerHTML = `     <h2>${country.name}</h2>
+                <div class="flex">
+                    <div class="content">
+                      <p>Capital: <span>${country.capital}</span></p>
+                      <p>Population: <span>${country.population}</span></p>
+                      <div>
+                        <p>Languages:</p>
+                        <ul>
+                            ${lol(country.languages)}
+                        </ul>
+                      </div>
+                   </div>
+          
+                   <img src=${country.flag} alt="">
+              </div>`;
+                              
+            }
+            
+        })
+    }
+   
 
 }
 function alertError(){
@@ -63,4 +78,5 @@ function createGalleryList(data){
 function lol(langs){
     return langs.reduce((acc,el) => acc+=`<li>${el.nativeName}</li>`,'');
 }
+
 
